7d712e9159f37b5ee5ff7da3510ccb269a6b70c3
P Lowe <perun@reagan.com>
2019-07-01 23:57:38 -0400

    MVP wohoo

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 console.log('Intiate Load Sequence');
-  let countryInfo= []
-
+let countryInfo= []
+let userInput = ''
+let countryCapital = null
+let countryRegion = null
+let countryLanguage = null
+let countryPop = null
+let countryDol = null
   function renderCountry(countryInfo, userInput){
     //   console.log('Country Information');
     //   console.log(countryInfo) 
@@ -8,15 +13,21 @@ console.log('Intiate Load Sequence');
           for (let i = 0; i< countryInfo.length;i++){
               let countryName = countryInfo[i].name;
               if (userInput.toLowerCase() == countryName.toLowerCase()){
-              let countryCapital = countryInfo[i].capital; 
-              let countryRegion = countryInfo[i].region;
-              let countryLanguage = countryInfo[i].languages[0].name
+            //   let countryCapital = countryInfo[i].capital; 
+               countryCapital = countryInfo[i].capital; 
+               console.log('capital is: ' + countryCapital)
+              countryRegion = countryInfo[i].region;
+              countryLanguage = countryInfo[i].languages[0].name
               //another object and array
               let countryFlag = countryInfo[i].flag
-              let countryPop = formatN(countryInfo[i].population)
-              let countryDol = countryInfo[i].currencies[0].name
+              countryPop = formatN(countryInfo[i].population)
+              countryDol = countryInfo[i].currencies[0].name
+              let countryLL = countryInfo[i].latlng
+              console.log(countryLL)
+              initMap(countryLL[0],countryLL[1])
+            // initMap()
               //another object and array
-              let countryZone = countryInfo[i].timezones
+            //   let countryZone = countryInfo[i].timezones
                 // let image = $(`<img src='${countryFlag}'>`)
                 // console.log(countryName); 
                 $('#flag img').attr('src',countryFlag)
@@ -25,11 +36,12 @@ console.log('Intiate Load Sequence');
                 $('#capital').text("Capital: "+countryCapital)
                 $('#region').text("Region: "+countryRegion)
                 $('#languages').text("Native Language: "+countryLanguage)
-                // $('#timezone').text("Time Zone: "+countryZone)
                 $('#population').text("Population: "+countryPop)
                 $('#currency').text("Currancy: "+countryDol)
             //   console.log(countryCapital);
             //   console.log(countryRegion);
+                // $('#country').onclick(`window.open('https://en.wikipedia.org/wiki/${userInput}')`)
+                // $('#country').title("Info on " +countryName)
               }
           }
       }
@@ -42,10 +54,9 @@ function formatN (num){
 
   $('#country-search').submit(function(event){
       event.preventDefault()
-      let userInput = $('#search-term').val()
+      userInput = $('#search-term').val()
       $('#search-term').val("")
       $.ajax({
-        //   url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key${userInput}&=AIzaSyAuPk8bH7WX4-_brj71pAFx6HljsJUFS84`,
         url: `https://restcountries.eu/rest/v2/all`,  
         success: function(response){  
             // console.log(response);   
@@ -65,17 +76,72 @@ function formatN (num){
   })
 //onhover pull ajax for tool
 
-// function callDataC (userInput) {
-// document.getElementById('country2').click(
+// function changeTitle (){
+//     $('#country').title("Info on " +countryName)
+//     // $('#capital').title(countryCapital)
+//     // $('#region').title(countryRegion)
+//     // $('#languages').title(countryLanguage)
+//     // $('#population').title(countryPop)
+//     // $('#currency').title(countryDol)       
+// }
+
+function nWin() {
+    let windowO = window.open(`https://en.wikipedia.org/wiki/${userInput}`, '_blank');
+    }
+
+function nWin2 (){
+    // console.log(event)
+    // console.log(event.data)
+    // const data = event.data.param
+    let windowOpen = window.open(`https://en.wikipedia.org/wiki/${countryCapital}`, '_blank')
+}
+function nWin3 (){
+    let windowOpen = window.open(`https://en.wikipedia.org/wiki/${countryRegion}`, '_blank')
+}
+function nWin4 (){
+    let windowOpen = window.open(`https://en.wikipedia.org/wiki/${countryLanguage}`, '_blank')
+}
+function nWin5 (){
+    let windowOpen = window.open(`https://en.wikipedia.org/wiki/population`, '_blank')
+}
+function nWin6 (){
+    let windowOpen = window.open(`https://en.wikipedia.org/wiki/${countryDol}`, '_blank')
+}
+    // 
+    function initMap(latitude,longitude) {
+        // console.log(latitude);
+        // console.log(longitude);
+        console.log('in init map')
+        const element = document.getElementById('map')
+        console.log(element)
+
+        
+      let map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng( latitude,  longitude),
+        zoom: 5
+      });
+      console.log(map);
+      
+    }
+// function callData (userInput){
+//     $('#country').onclick(`window.open('https://en.wikipedia.org/wiki/${userInput}')`)
+//     // $('#capital').title(countryCapital)
+//     // $('#region').title(countryRegion)
+//     // $('#languages').title(countryLanguage)
+//     // $('#population').title(countryPop)
+//     // $('#currency').title(countryDol)  
+// }
 
 // )
 // }
 
 // $('#country2').on('click', callDataC)
-// $('#country').on('click', callData)
-// $('#capital').on('click', callData)
-// $('#region').on('click', callData)
-// $('#languages').on('click', callData)
+$('#country').on('click', nWin)
+// console.log(countryCapital);
+// $('#capital').on('click', {param: countryCapital}, nWin2)
+$('#capital').on('click', nWin2)
+$('#region').on('click', nWin3)
+$('#languages').on('click', nWin4)
 // $('#timezone').on('click', callData)
-// $('#population').on('click', callData)
-// $('#currency').on('click', callData)
+$('#population').on('click', nWin5)
+$('#currency').on('click', nWin6)
